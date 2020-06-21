1e5dbf25d7d6afe9841f422c1e7e3d8a295e734a
thesaintzion <saintzion777@gmail.com>
2020-06-21 14:25:02 +0100

    added animation on slash screen...

---
+++ app/src/main/java/com/lekkiforum/cashcoin/ActivityCalculator.kt
@@ -0,0 +1,27 @@
+package com.lekkiforum.cashcoin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_calculator.*
+
+
+class ActivityCalculator : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+        Toast.makeText(this,"Welcome Back Home", Toast.LENGTH_LONG).show()
+
+
+    }
+
+
+
+
+
+
+
+}
